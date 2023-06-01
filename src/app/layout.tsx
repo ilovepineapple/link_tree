5066f19447b48ed4c924a07cb201c6cef84faba3
Pineapple <pineappleman@skiff.com>
2023-05-31 21:26:40 -0300

    fix: remove description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,8 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Lucas Moser Oficial',
-  description: 'Bem-vindo ao site oficial de Lucas Moser! Aqui você encontrará o universo musical do renomado guitarrista, consultor e entusiasta dos pedais de efeito. Explore suas performances cativantes, assista a vídeos exclusivos, confira seus reviews de equipamentos e mergulhe em sua expertise como consultor especializado em HX Stomp e pedalboards. Prepare-se para uma jornada envolvente pelo mundo da guitarra e do blues. Conecte-se conosco e desbloqueie novas possibilidades musicais. Bem-vindo ao som de Lucas Moser!',
+  title: ' Oficial',
+  description: 'Bem-vindo ao site oficial de ...! Aqui você encontrará o universo musical do renomado guitarrista, consultor e entusiasta dos pedais de efeito. Explore suas performances cativantes, assista a vídeos exclusivos, confira seus reviews de equipamentos e mergulhe em sua expertise como consultor especializado em HX Stomp e pedalboards. Prepare-se para uma jornada envolvente pelo mundo da guitarra e do blues. Conecte-se conosco e desbloqueie novas possibilidades musicais. Bem-vindo ao som de Lucas Moser!',
 }
 
 export default function RootLayout({
